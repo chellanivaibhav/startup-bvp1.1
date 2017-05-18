609f51e75f526981e0accb58df100fac6f401b77
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-05-18 12:27:28 +0530

    logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,10 @@
 <?php 
 	session_start();
 	session_unset();
-
+	?><script type="text/javascript">
+        window.location = "http://localhost/startup-bvp1.1/index.php";
+      </script>  
+      <?php
 
 
 ?>
